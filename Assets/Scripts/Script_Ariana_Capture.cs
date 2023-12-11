4567b80bcd3986e71be5a26b4baaab3d256ce3cf
milktoastdev <finlayson.ariana@gmail.com>
2023-12-11 22:01:18 +1100

    I need everyone to see this dictionary I am a GODDDDD

--- Assets/Scripts/Script_Ariana_Capture.cs
+++ Assets/Scripts/Script_Ariana_Capture.cs
@@ -7,10 +7,13 @@ public class Script_Ariana_Capture : MonoBehaviour
     // The manager objects. These are important. Benevolent overlords, if you will.
     public GameObject _minigameManager;
     public GameObject _UIManager;
-
+    
     // Added in inspector in order Red, Green, Blue (RGB) (I'm creative)
     public List<GameObject> _captureButtons = new List<GameObject>();
-
+    // I just discovered dictionaries and my life has been changed forever
+    private Dictionary<GameObject, int> _buttonIndexDictionary = new Dictionary<GameObject, int>();
+    
+    // I might not need to use these hold on
     // Keeps track of which button the player pressed last. Starts at -1 because the first button a player can press is at index 0 in the list.
     private int _lastButtonIndex = -1;
     // Do I need to explain it again lol
@@ -19,9 +22,28 @@ public class Script_Ariana_Capture : MonoBehaviour
     // OnEnable is called before Start
     void OnEnable()
     {
-        Debug.Log("Yarghhhh I be awakened");
+        // Help I'm delusional
+        Debug.Log("Yarghhhh I be awakened matey");
+        
+        // Adds each button to the dictionary in order I can't believe this isn't throwing errors oh my fucking god
+        Debug.Log("Executing dictionary for loop...");
+        for(int i = -1; i < _captureButtons.Count; i++)
+        {
+            // int i ends up being the same as the index of the buttons in the list. I think. Coding math is hard.
+            Debug.Log($"Adding {_captureButtons[i]} key to dictionary with a value of {i}..."); 
+            _buttonIndexDictionary.Add(_captureButtons[i],i);
+            Debug.Log($"Added {_captureButtons[i]} key to dictionary with a value of {i}.");
+            // The fact that this is throwing no compile errors is astounding honestly
+        }
+        Debug.Log("Dictionary for loop executed.");
+                
+        // Starts the internal game loop
+        Debug.Log("Calling internal game loop...");
         CaptureStart();
-        Debug.Log("Starting the capture minigame...");
+        Debug.Log("Internal game loop called.");
+
+        // Sorry
+        Debug.Log("Ready to sail the high seas me matey yarghhhh");
     }
 
     // Starts the dang thang. You can't play a game you ain't started
@@ -39,14 +61,15 @@ public class Script_Ariana_Capture : MonoBehaviour
 
         // Can't play a button game without clicking buttons (the correct ones, specifically)
         Debug.Log("Starting button order coroutine...");
-        StartCoroutine(ButtonOrder());
         Debug.Log("Button order coroutine started.");
     }
     
     // Assigns the button the player pressed a variable to be able to be checked in the coroutine
-    void WhatButtonAmI()
+    int WhatButtonAmI(int myButtonIndex)
     {
-
+        
+        
+        return myButtonIndex;
     }
     
     IEnumerator ButtonOrder()
